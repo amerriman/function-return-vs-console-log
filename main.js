b30435c44faf0c26c9acc67e2b406869b320f6f7
amerriman <amm434@gmail.com>
2015-07-13 13:08:42 -0600

    include if else in solution

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 //Design an experiment to demonstrate the difference between returning a value in a function and console.logging it.
 
-function experimentA(x, y) {
+function experimentA(x,y) {
  console.log(x+y);
 };
 experimentA(1,1)
+
 
 function experimentB(x,y) {
   return x+y
 }
 experimentB(2, 2)
 //to see what experimentB is doing, you can console.log it outside of the function.
-console.log(experimentB(2,2));
+// console.log(experimentB(2,2));
+if (experimentB() === undefined){
+ console.log("function doesn't return value");}
+ else {
+   console.log("function returns value");
+ }
+
+ if (experimentA() === undefined){
+  console.log("function doesn't return value");}
+  else {
+    console.log("function returns value");}
